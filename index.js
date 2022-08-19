fe598a72ef8c5dd754f5cd1fe5ed44ebecd15a11
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-08-19 13:16:40 +0200

    creating async function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const productsUrl = "http://localhost/flower-power/wordpress-6.0.1/wordpress/home/wp-json/wc/store/products";
+
 async function getProducts(url) {
     const response = await fetch(url);
+    const products = await response.json();
+    console.log(products);
 }
+
+getProducts(productsUrl);
